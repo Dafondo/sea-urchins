98bd67e3fd9cc01cc87e2836309c36f88237f391
Cynthia <xcynthia@live.unc.edu>
2019-11-03 11:21:24 -0500

    slight updates to website

--- pill-time/main.js
+++ pill-time/main.js
@@ -1,20 +1,32 @@
 // without jQuery (doesn't work in older IEs)
 document.addEventListener('DOMContentLoaded', function(){ 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", '/gsk/new', true);
+    xhr.open("POST", 'http://m.jbakita.me:9000/gsk/new', true);
     xhr.send();
 
 }, false);
 
+function updatePillTable(pillType) {
+    var sidebar = document.getElementById("pill-table");
+    var row = sidebar.insertRow(-1);
+    var cell = row.insertCell(0);
+    cell.appendChild(pillType)
+}
+
 function updatePills() {
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", '/gsk/pills', true);
-    xhr.onload = function () {
-        
-    };
+    alert("in12");
+    xhr.open("GET", 'http://m.jbakita.me:9000/gsk/pills', true);
+    xhr.responseType = "text";
+    alert("in13");
+    xhr.onload = function() {
+        updatePillTable(xhr.response);
+    }
     xhr.send();
 }
 
 function saveNewPill() {
-    document.getElementById("pillForm").submit();
+    //document.getElementById("pillForm").submit();
+    alert("in");
+    updatePills();
 }
